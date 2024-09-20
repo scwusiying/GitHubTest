73a3e0209cb1fb955848f22a6bd3fba4b5ff08b0
WuSiYing <scwusiying@gmail.com>
2024-09-20 08:57:18 +0800

    新增button1功能

--- GitHubTest/Form1.cs
+++ GitHubTest/Form1.cs
@@ -16,5 +16,10 @@ namespace GitHubTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "歡迎";
+        }
     }
 }
